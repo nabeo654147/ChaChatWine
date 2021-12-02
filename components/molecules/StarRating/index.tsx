99a1c7665638306f790299f7f23a178d0f1de40e
nabeo654147 <nabehiko616@gmail.com>
2021-12-02 19:44:54 +0900

    Raing,Range,Uploader修正

--- components/molecules/StarRating/index.tsx
+++ components/molecules/StarRating/index.tsx
@@ -1,82 +1,66 @@
-import React, { VFC } from "react";
-import styled, { css } from "styled-components";
+import React, { FC } from 'react';
+import styled, { css } from 'styled-components';
 
-type RatingInputProps = {
-  type?: string,
-  id?: string,
-  name?: string,
-  value?: string,
-  htlmFor?: string,
-  title?: string,
-  ariaHidden?: boolean,
-  ratingTitle?: string,
-  starIds?: string[]
-}
+type Props = {
+  ratingTitle?: string;
+  starIds?: string[];
+  favorite: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-export const StarRating:VFC<RatingInputProps> = ({ 
-  ratingTitle,
-  starIds = ['5star','4star','3star','2star','1star',]
- }) => {
+export const StarRating: FC<Props> = ({ ratingTitle, starIds, favorite, onChange }) => {
   return (
     <RatingWrap>
-    <label>{ratingTitle}</label>
-    <Rating>
-      {starIds.map((starId) => {
-        return (
-          <React.Fragment key={starId}>
-            <RatingInput 
-              type="radio"
-              id={starId}
-              name="rating"
-              value={starId} 
-            />
-              <RatingLabel 
-                htmlFor={starId}
-                title={starId}
-              >
-                <RatingIcon aria-hidden="true">
-                </RatingIcon>
-              <HiddenVisually>{starId}</HiddenVisually>
-            </RatingLabel>
-          </React.Fragment>
-        )
-      })}
-    </Rating>
-
+      <label>{ratingTitle}</label>
+      <div>
+        {starIds?.map((starId) => {
+          return (
+            <React.Fragment key={starId}>
+              <RatingInput
+                type='radio'
+                id={starId}
+                name='rating'
+                value={starId}
+                checked={starId === favorite}
+                onChange={onChange}
+              />
+              <RatingLabel htmlFor={starId} title={starId}>
+                <StarIcon aria-hidden='true'></StarIcon>
+                <HiddenVisually>{starId}</HiddenVisually>
+              </RatingLabel>
+            </React.Fragment>
+          );
+        })}
+      </div>
     </RatingWrap>
-  )
+  );
 };
 
 const RatingWrap = styled.div`
   display: flex;
   justify-content: space-between;
-`
-
-const Rating = styled.div`
-  /* display: inline-flex;
-  flex-direction: row-reverse; */
-`
+`;
 
 const HiddenStyle = css`
-    border: 0;
-    clip: rect(1px 1px 1px 1px);
-    clip: rect(1px, 1px, 1px, 1px);
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-    width: 1px;
-`
+  border: 0;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+`;
 const HiddenVisually = styled.span`
   ${HiddenStyle}
-`
+`;
 
 const RatingLabel = styled.label`
   cursor: pointer;
   color: gray;
   padding: 0 0.25rem;
-`
+`;
 
 const RatingInput = styled.input`
   ${HiddenStyle}
@@ -86,11 +70,11 @@ const RatingInput = styled.input`
   &:checked ~ ${RatingLabel} {
     color: #ffbb00;
   }
-`
+`;
 
-const RatingIcon = styled.span`
+const StarIcon = styled.span`
   ::before {
-    content: "★";
+    content: '★';
     font-size: 2rem;
   }
-`
+`;
--- components/organisms/Uploader/index.tsx
+++ components/organisms/Uploader/index.tsx
@@ -1,106 +1,42 @@
-import { getDownloadURL, ref, uploadBytesResumable } from '@firebase/storage';
-import { TaskState } from 'firebase/storage';
-import  React,{ useState, useCallback, FC } from 'react';
-import styled from 'styled-components';
-import { storage } from '../../../lib/firebase';
+import React, { useState, useCallback, FC, SetStateAction, Dispatch } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Image from 'next/dist/client/image';
+import { getDownloadURL, ref, uploadBytesResumable } from '@firebase/storage';
+import { getAuth } from '@firebase/auth';
+import { TaskState } from 'firebase/storage';
+import { storage } from '../../../lib/firebase';
+import styled from 'styled-components';
 
-export type firebaseOnLoadProp = {
-  bytesTransferred: number;
-  totalBytes: number;
-  state: TaskState;
+type uploadProps = {
+  src: string;
+  setSrc: Dispatch<SetStateAction<string>>;
+  myFiles: File[];
+  setMyFiles: Dispatch<SetStateAction<File[]>>;
 };
 
-const Upload: FC = () => {
-  const [myFiles, setMyFiles] = useState<File[]>([]);
-  const [clickable, setClickable] = useState<boolean>(false);
-  const [src, setSrc] = useState<string>('');
+export const Uploader: FC<uploadProps> = ({ src, myFiles, setMyFiles, setSrc }) => {
+  const currentUser = getAuth().currentUser;
 
-  const onDrop = useCallback( async(acceptedFiles: File[]) => {
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
     if (!acceptedFiles[0]) return;
 
     try {
       setMyFiles([...acceptedFiles]);
-      setClickable(true);
       handlePreview(acceptedFiles);
     } catch (error) {
       alert(error);
       console.error(error);
     }
-  },[]);
+  }, []);
 
   const onDropRejected = () => {
     alert('無効なファイルです。');
   };
 
-  const  { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    onDropRejected
-  })
-
-  const handleUpload = (acceptedImg: any) => {
-    try {
-      const storageRef = ref(storage, (`imgaes/${myFiles[0].name}`));
-      const uploadTask = uploadBytesResumable(storageRef, myFiles[0]);
-      
-      uploadTask.on(
-        'state_changed', 
-        (snapshot: firebaseOnLoadProp) => {
-          const progress: number = 
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`);
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-            case 'running':
-              console.log('Upload is running');
-              break;
-          }
-        },
-        (error: any) => {
-          //失敗した場合
-          switch (error.code) {
-            case 'storage/unauthorized':
-              console.error('権限がありません');
-              break;
-            case 'storage/canceled':
-              console.error('アップロードがキャンセルされました');
-              break;
-            case 'storage/unknown':
-              console.error('予期せぬエラーが発生しました');
-              break;
-          }
-        },
-        () => {
-          //成功した時
-          try {
-            getDownloadURL(uploadTask.snapshot.ref)
-              .then((downloadUrl: string) => {
-                console.log(`ダウンロードしたURL ${downloadUrl}`);
-              });
-          } catch (error: any) {
-            switch (error.code) {
-              case 'storage/object-not-found':
-                console.log('ファイルが存在しませんでした');
-                break;
-              case 'storage/unauthorized':
-                console.log('権限がありません');
-                break;
-              case 'storage/canceled':
-                console.log('キャンセルされました');
-                break;
-              case 'storage/unknown':
-                console.log('予期せぬエラーが生じました');
-                break;
-            }
-          }
-        }
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    onDropRejected,
+  });
 
   const handlePreview = (files: any) => {
     const file = files[0];
@@ -115,62 +51,37 @@ const Upload: FC = () => {
 
   return (
     <Outline>
-      <UploadBox
-        {...getRootProps()}
-      >
+      <UploadBox {...getRootProps()}>
         {/* この中をタップすれば画像を選択できる */}
         <input {...getInputProps()} />
         {myFiles.length === 0 ? (
-          <GuideText>画像を選択またはドラッグ＆ドロップできます</GuideText>
+          <p>画像を選択またはドラッグ＆ドロップできます</p>
         ) : (
           <div>
             {myFiles.map((file: File) => (
               <React.Fragment key={file.name}>
-                {src && <Image src={src} width={350} height={400} />}
+                {src && <Image src={src} width={500} height={600} layout={'intrinsic'} />}
               </React.Fragment>
             ))}
           </div>
         )}
       </UploadBox>
-      <UploadButton
-        disabled={!clickable}
-        type="submit"
-        onClick={() => handleUpload(myFiles)}
-        >
-          UPLOAD
-        </UploadButton>
-      </Outline>
+    </Outline>
   );
 };
 
-export default Upload;
-
 const Outline = styled.div`
-  width: 80%;
   padding: 0.5rem 1rem;
-  margin: 0 auto;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  /* text-align: center; */
-  border-radius: 5px;
-`
+`;
 
 const UploadBox = styled.div`
-  /* height: 50vh; */
   background-color: #c6dbff;
-  border: 2px solid #6B7280;;	
+  border: 2px solid #6b7280;
   border-radius: 5px;
-`
-
-const GuideText = styled.p`
-  padding: 1rem 0;
-  display: flex;
-  justify-content: center;
-`
-
-const UploadButton = styled.button`
-  padding: 0.5rem 1rem;
-  margin: 1rem 0;
-  border-radius: 5px;
-`
+  p {
+    padding: 1rem;
+    text-align: center;
+  }
+`;
