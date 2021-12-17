4eefed79cbfdbc91d140cea86f28b902cbff3e98
nabeo654147 <nabehiko616@gmail.com>
2021-12-17 23:20:30 +0900

    匿名ログインの排除&リダイレクト機能修正

--- lib/AuthContext.tsx
+++ lib/AuthContext.tsx
@@ -1,14 +1,14 @@
 import { createContext, useState, useEffect, useContext } from 'react';
 
 import { auth } from '../lib/firebase';
-import { createUserWithEmailAndPassword, getAuth, User, UserCredential } from '@firebase/auth';
-import { signInWithEmailAndPassword, signInAnonymously } from 'firebase/auth';
+import { createUserWithEmailAndPassword, User, UserCredential } from '@firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 type AuthContextType = {
+  currentUser: User | null;
   signUp: (email: string, password: string) => Promise<UserCredential>;
   login: (email: string, password: string) => Promise<UserCredential>;
   logout: () => Promise<void>;
-  isAnonymous?: boolean;
 };
 
 type Props = {
@@ -31,36 +31,29 @@ const logout = () => {
   return auth.signOut();
 };
 
-const AuthContext = createContext<AuthContextType>({ signUp, logout, login });
+const AuthContext = createContext<AuthContextType>({ currentUser: null, signUp, logout, login });
 
-//プロバイダーは状態を管理するものなので、変わらないsignUpなどは別でutilなどに切り出しても良いかも
 const AuthProvider = ({ children }: Props): JSX.Element => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const isAnonymous = getAuth().currentUser?.isAnonymous;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    return auth.onAuthStateChanged(async (user: User | null) => {
-      if (!user) {
-        signInAnonymously(auth);
-      } else {
-        setCurrentUser(user);
-        setIsLoading(false);
-      }
+    return auth.onAuthStateChanged((user: User | null) => {
+      setCurrentUser(user);
+      setIsLoading(false);
     });
   }, []);
 
   const value: AuthContextType = {
+    currentUser,
     signUp,
     login,
     logout,
-    isAnonymous,
   };
 
   return (
     <AuthContext.Provider value={value}>
-      {isLoading ? <p>Loding...</p> : children}
+      {isLoading ? <div suppressHydrationWarning>Loding...</div> : children}
     </AuthContext.Provider>
   );
 };
